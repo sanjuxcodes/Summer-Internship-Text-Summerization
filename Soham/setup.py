b2782b8652226c9aea87b3bf0affebeb3f47ad69
CODESSOHAM <sohamaich06@gmail.com>
2026-06-13 18:28:42 +0530

    initial try

---
+++ Soham/setup.py
@@ -0,0 +1,7 @@
+import nltk
+
+nltk.download('punkt')
+nltk.download('punkt_tab')
+nltk.download('stopwords')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('averaged_perceptron_tagger_eng')
